0e0c150a678c136c92f9415b6bcefa8f194b450e
HoangLight2006 <hoanglightvn@gmail.com>
2026-03-30 13:58:51 +0700

    Add project files.

---
+++ TeeLab/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TeeLab.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ TeeLab/Models/Nguoi.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TeeLab.Models
+{
+    public class Nguoi
+    {
+        [Key]
+        public int NguoiId { get; set; }
+        public string? Hoten { get; set; }
+
+    }
+}
